0507785004315921b7b86678cc65b561a72a88ed
pliniocode <pliniohrcode@gmail.com>
2022-12-06 20:40:48 -0300

    add product already

--- src/controllers/newProductController.js
+++ src/controllers/newProductController.js
@@ -1,7 +1,17 @@
+import { addProduct } from '../services/services.js';
 
+const form = document.querySelector('[data-newProduct]');
 
-document.querySelector('[data-newProduct]')
-  .addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event);
+    const urlImage = event.target.querySelector('[data-type="image"]').value;
+    const category = event.target.querySelector('[data-type="category"]').value;
+    const productName = event.target.querySelector('[data-type="productName"]').value;
+    const price = event.target.querySelector('[data-type="price"]').value;
+    const description = event.target.querySelector('[data-type="description"]').value;
+
+    addProduct(urlImage, category, productName, price, description)
+    .then(() => {
+      alert('Cadastro concluído 🤩')
+    });
 });
--- src/services/services.js
+++ src/services/services.js
@@ -9,3 +9,22 @@ export function searchProducts(query) {
   return fetch(`http://localhost:3000/produtos?q=${query}`)
     .then((response) => response.json());
 }
+
+export function addProduct(urlImage, category, productName, price, description) {
+  return fetch(`http://localhost:3000/produtos`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      urlImagem: urlImage,
+      categoria: category,
+      nome: productName,
+      preco: price,
+      descricao: description
+    })
+  })
+    .then((response) => {
+      return response.body
+    })
+}
